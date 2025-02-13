7d7c5d7260f6275fb5ea919778538ea0628ef9f7
Jake Edwardson <edward72@lancaster.ac.uk>
2025-02-13 20:26:40 +0000

    changed to data to focus on a specific team inputted by the user, and started to create evaluation for the model in which i hope to improve it in the future

--- PremierLeaguePredictor.py
+++ PremierLeaguePredictor.py
@@ -11,7 +11,9 @@ team_data = data[data['team'] == team_name]
 
 #split the data into training and testing data
 #data sorted by end year, oldest at the start
-split_year = 2019
+latest_year = data['season_end_year'].max()
+n = 5
+split_year = latest_year - n
 training_data = team_data[team_data["season_end_year"] < split_year]
 testing_data =  team_data[team_data["season_end_year"] > split_year]
 
@@ -21,10 +23,20 @@ x_test = testing_data[['gd', 'points']]
 #define target
 y_train = training_data['position']
 y_test = testing_data['position']
+#convert y_test to a numpy array to later be compared to the predictions
+y_test = y_test.to_numpy()
 
 #create the model and train it using linear regression
 model = LinearRegression()
 model.fit(x_train, y_train)
 
 y_pred = model.predict(x_test)
-print(y_pred)
+#round the predictions to the nearest in as league postion cannot be a decimal
+y_pred = np.round(y_pred).astype(int)
+
+#calculate the accuracy of the model
+#Mean Absolute Error
+mae = 0
+for i in range(n):
+    mae += abs(y_test[i] - y_pred[i])
+print(f"MAE = {mae}")
