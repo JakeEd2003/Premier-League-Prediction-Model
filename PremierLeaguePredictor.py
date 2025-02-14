5163434f0ff1fc92be3ddbe2ea948d5421d9b919
Jake Edwardson <edward72@lancaster.ac.uk>
2025-02-14 14:03:37 +0000

    Added Random Forest regressor to compare accuracy due to the numerival values. Need to add more measures for accuracy. Also added a separate file containing the code for each of the models and the training and test data separation.

--- PremierLeaguePredictor.py
+++ PremierLeaguePredictor.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
+import regressionModels
 
+n=5
 filename = 'pl-tables-1993-2024.csv'
 data = pd.read_csv(filename)
 team_name = input("Enter a team name: ")
@@ -9,37 +11,15 @@ team_name = input("Enter a team name: ")
 #get data of selected team
 team_data = data[data['team'] == team_name]
 
-#split the data into training and testing data
-#data sorted by end year, oldest at the start
-latest_year = data['season_end_year'].max()
-n = 5
-split_year = latest_year - n
-training_data = team_data[team_data["season_end_year"] < split_year]
-testing_data =  team_data[team_data["season_end_year"] > split_year]
-
-#define features
-x_train = training_data[['gd', 'points']]
-x_test = testing_data[['gd', 'points']]
-#define target
-y_train = training_data['position']
-y_test = testing_data['position']
-#convert y_test to a numpy array to later be compared to the predictions
-y_test = y_test.to_numpy()
-
-#create the model and train it using linear regression
-model = LinearRegression()
-model.fit(x_train, y_train)
-
-y_pred = model.predict(x_test)
-#round the predictions to the nearest in as league postion cannot be a decimal
-y_pred = np.ceil(y_pred).astype(int)
+#y_test, y_pred = regressionModels.linearRegression(data, team_data)
+y_test, y_pred = regressionModels.random_forest_regressor(data, team_data)
 
 #calculate the accuracy of the model
 #Mean Absolute Error
 mae = 0
-print(y_test) #DELETE IN FUTURE
-print(y_pred) #DELETE IN FUTURE
 for i in range(n):
     mae += abs(y_test[i] - y_pred[i])
 mae = mae/n
 print(f"MAE = {mae}")
+
+#ADD MORE ACCURACY MEASURES (MEAN SQUARED ERROR, R2)
---
+++ regressionModels.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+
+
+def random_forest_regressor(data, team_data):
+    x_train, x_test, y_train, y_test = split_data(data, team_data)
+    rfr = RandomForestRegressor(random_state=42)
+    rfr.fit(x_train, y_train)
+    y_pred = rfr.predict(x_test)
+    y_pred = np.ceil(y_pred).astype(int)
+    return y_test, y_pred
+
+def linearRegression(data, team_data):
+    x_train, x_test, y_train, y_test = split_data(data, team_data)
+    model = LinearRegression()
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    y_pred = np.ceil(y_pred).astype(int)
+    return y_test, y_pred
+
+def split_data(data, team_data):
+
+    latest_year = data['season_end_year'].max()
+    n = 5
+    split_year = latest_year - n
+    training_data = team_data[team_data["season_end_year"] < split_year]
+    testing_data =  team_data[team_data["season_end_year"] > split_year]
+
+    #define features
+    #x_train = training_data[['gd', 'points']]
+    #x_test = testing_data[['gd', 'points']]
+    x_train = training_data[['position']]
+    x_test = testing_data[['position']]
+    #define target
+    y_train = training_data['position']
+    y_test = testing_data['position']
+    #convert y_test to a numpy array to later be compared to the predictions
+    y_test = y_test.to_numpy()
+    return x_train, x_test, y_train, y_test
